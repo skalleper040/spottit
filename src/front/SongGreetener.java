645f68fbcde3b2cc6ba998feda39493a9cc1932a
Oscar Malmqvist <o.malmqvist@gmail.com>
2019-01-10 15:10:47 +0100

    refactor exceptionhantering

--- src/front/SongGreetener.java
+++ src/front/SongGreetener.java
@@ -20,44 +20,68 @@ import entity.Song;
 @Path("/song")
 public class SongGreetener {
 
-	@GET
-	@Produces("text/json")
-	public Response getSongInfo(@QueryParam("artist") String artist, @QueryParam("title") String title, @QueryParam("trackUri") String trackUri, @QueryParam("coverUri") String coverUri) {
-
-		System.out.println("getting a song");
-		Song song = new Song(artist, title, trackUri, coverUri);
-
+	private String retrieveSong(Song song) {
+		String jsonInString = null;
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
-		
-		if (song.validate()) {
 
-			String jsonInString = "";
+		if (song.validate()) {
 			try {
 				jsonInString = mapper.writeValueAsString(song);
 			} catch (JsonProcessingException e) {
 				e.printStackTrace();
 			}
+		}
+		return jsonInString;
+	}
 
-			return Response.ok()
-					.entity(jsonInString)
-					.header("Access-Control-Allow-Origin", "*")
-					.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
-					.build();
+	@GET
+	@Produces("text/json")
+	public Response getSongInfo(@QueryParam("artist") String artist, @QueryParam("title") String title, @QueryParam("trackUri") String trackUri, @QueryParam("coverUri") String coverUri) {
+		JSONObject json = new JSONObject(); // Creates an object used for response-build
+
+		if (artist == null || title == null) { // Check that we got both artist and title to look for
+			try {
+				json.put("Status", "error");
+				json.put("Code", 400);
+				json.put("Message", "Bad request. Please provide correct artist and title");
+				return Response.status(Response.Status.BAD_REQUEST)
+						.entity(json.toString())
+						.header("Access-Control-Allow-Origin", "*")
+						.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+						.build();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
-		// Create an error message and return it if the song isnt available.
-		JSONObject json = new JSONObject();
-		try {
+		// All good, create a song
+		Song song = new Song(artist, title, trackUri, coverUri);
+		String entity = null;
 
-			json.put("Status", "error");
-			json.put("Code", 404);
-			json.put("Message", "Song not found or not all hints for song available");
-		} catch (JSONException e) {
+		if (song.validate()) { // Check that the song got all info it needs to be playable
+			entity = retrieveSong(song);
 		}
-		
-		return Response.status(404)
-				.entity(json.toString())
+
+		if (entity == null) { // If something went wrong with the hints
+			try {
+				json.put("Status", "error");
+				json.put("Code", 404);
+				json.put("Message", "Song not found or not all hints for song available");
+				return Response.status(Response.Status.NOT_FOUND)
+						.entity(json.toString())
+						.header("Access-Control-Allow-Origin", "*")
+						.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+						.build();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return Response.ok()
+				.entity(entity)
 				.header("Access-Control-Allow-Origin", "*")
 				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
 				.build();
