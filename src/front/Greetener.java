a7c6973edb3365172d17a6fd71ec25b978d21803
Oscar Malmqvist <o.malmqvist@gmail.com>
2019-01-10 15:26:00 +0100

    exceptionhandle i playlist

--- src/front/Greetener.java
+++ src/front/Greetener.java
@@ -8,6 +8,9 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import backend.CreatePlaylistFromSpotify;
 import entity.SpotifyPlaylist;
 
@@ -18,21 +21,32 @@ public class Greetener {
 	@Produces("text/json")
 
 	public Response getPlaylist(@QueryParam("id") String playlistId, @QueryParam("accessToken") String accessToken) {
+		JSONObject json = new JSONObject(); // Creates an object used for response-build
 		System.out.println(accessToken);
+
 		CreatePlaylistFromSpotify cpfs = new CreatePlaylistFromSpotify();
-
-		if (cpfs.checkResponse(playlistId, accessToken) == 200) {
-
-			String playlistAsJson = cpfs.getPlaylist(playlistId, accessToken);
-
-			return Response.ok() //200
-					.entity(playlistAsJson)
-					.header("Access-Control-Allow-Origin", "*")
-					.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
-					.build();
+		int responseCode = cpfs.checkResponse(playlistId, accessToken);
+		
+		// If something went wrong when fetching playlist from spotify
+		if (responseCode != 200) {
+			try {
+				json.put("Status", "error");
+				json.put("Code", responseCode);
+				return Response.status(responseCode)
+						.entity(json.toString())
+						.header("Access-Control-Allow-Origin", "*")
+						.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+						.build();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-
-		return Response.status(cpfs.checkResponse(playlistId, accessToken))
+		
+		// All good, get the playlist and return it as Json
+		String playlistAsJson = cpfs.getPlaylist(playlistId, accessToken);
+		return Response.ok() //200
+				.entity(playlistAsJson)
 				.header("Access-Control-Allow-Origin", "*")
 				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
 				.build();
